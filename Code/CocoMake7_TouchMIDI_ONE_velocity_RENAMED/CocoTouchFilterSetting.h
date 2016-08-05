3f318ef658cd5c0ad0fa30a1285bbb8004e6484d
Marc Dusseiller <marc@dusseiller.ch>
2016-08-05 12:22:37 +0200

    Add files via upload

---
+++ Code/CocoMake7_TouchMIDI_ONE_velocity_RENAMED/CocoTouchFilterSetting.h
@@ -0,0 +1,19 @@
+
+
+#define SAMPLEFILTER_TAP_NUM 10
+
+static int filter_taps[SAMPLEFILTER_TAP_NUM] = {
+  8,
+  39,
+  97,
+  168,
+  217,
+  217,
+  168,
+  97,
+  39,
+  8
+};
+
+
+#include "CocoTouchFilter.h"
