e248892639b8fdcf429f8e7fada833ebdbe94389
dusjagr <marc@dusseiller.ch>
2016-08-18 13:33:34 +0200

    cleaning up

--- Code/CocoMake7_TouchMIDI_ONE_velocity_RENAMED/CocoMake7_TouchMIDI_ONE_velocity_RENAMED.ino
+++ Code/CocoMake7_TouchMIDI_ONE_velocity_RENAMED/CocoMake7_TouchMIDI_ONE_velocity_RENAMED.ino
@@ -1,5 +1,6 @@
-#define USE_KEYBOARD
+#define USE_MIDI
 #define USE_VELO
+
 #ifdef USE_MIDI
 #include <CocoMidi.h>
 MIDIMessage midimsg;
@@ -9,13 +10,13 @@ MIDIMessage midimsg;
 #include <CocoKeyboard.h>
 #endif
 
+
 #include <CocoTouch.h>
 #include "CocoTouchFilterSetting.h"
-    
-//char key[] = {'C','O','C','O','M','A','K','E','7',' ','O','N','E','!',' '};
-char key[] = {' '};
-int keyTotal = 1;
 
+char key[] = {'C','O','C','O','M','A','K','E','7',' ','O','N','E','!',' '};
+
+//TeenyTouchDusjagr test2;
 // ATMEL ATTINY85
 //
 //                   +-\/-+
@@ -33,19 +34,21 @@ int value[8] = {0,0,0,0,0,0,0,0};
 #ifdef USE_VELO
 int prevValue[8] = {0,0,0,0,0,0,0,0};
 int velocityValue[8] = {0,0,0,0,0,0,0,0};
-int prevVelocity[8] =  {0,0,0,0,0,0,0,0};
 #endif
 
 uint8_t note_off[8] = {1,1,1,1,1,1,1,1};
 uint16_t offset_adc[8] = {0,0,0,0,0,0,0,0};
+
+//filter settings
+
 CocoTouchFilterSetting CocoFilter[8];
 int filtered_value = 0;
 uint8_t pin_queue = 0;
 uint8_t multiplexer_mapping[8]  = {6,7,4,4,3,0,1,2}; //remap multiplexer pin
 unsigned long previousMillis = 0;        // will store last time LED was updated
 int keyCount = -1;
+int keyTotal = 15;
 int ledPin = PB0;
-int velocityThreshold = 80;
 
 #define PIN_SELECT 0
 #define NUM_CHANNEL 3
@@ -63,20 +66,11 @@ void setup()
 {
 
 #ifdef USE_MIDI
-    TeenyMidi.init();
+    CocoMidi.init();
 #endif
 
 #ifdef USE_KEYBOARD
     CocoKeyboard.update();
-    pinMode(ledPin, OUTPUT);
-    digitalWrite(ledPin, HIGH); 
-    CocoKeyboard.delay(100);        
-    digitalWrite(ledPin, LOW); 
-    CocoKeyboard.delay(100); 
-    digitalWrite(ledPin, HIGH); 
-    CocoKeyboard.delay(100);         
-    digitalWrite(ledPin, LOW); 
-    CocoKeyboard.delay(100);
 #endif
 
 
@@ -87,9 +81,11 @@ void setup()
     CocoTouch.usb_poll = &usb_poll;
 
     offset_adc[0] = CocoTouch.sense(ADC_SENSE_PIN,ADC_REF_PIN, 8 );
+    //offset_adc[0] = 0;
 
 #ifdef USE_MIDI
     CocoMidi.delay(100);
+#endif
     pinMode(ledPin, OUTPUT);
     digitalWrite(ledPin, HIGH); 
     CocoMidi.delay(100);        
@@ -99,46 +95,63 @@ void setup()
     CocoMidi.delay(100);         
     digitalWrite(ledPin, LOW); 
     CocoMidi.delay(100);
-#endif
-
-
-
 
 }
-          
+
 void loop()
 {
 
     if (millis()-previousMillis >= 5)     // 0% data loss
         {
+            //CocoMidi.sendCCHires(value, 1);
             filtered_value = CocoTouchFilter_get(&CocoFilter[0]);
-            velocityValue[0] = filtered_value - prevValue[0]+500;
+            velocityValue[0] = filtered_value - prevValue[0];
             prevValue[0] = filtered_value;
-            
-            if (velocityValue[0]  >= 500 + velocityThreshold && prevVelocity[0] <= 500 + velocityThreshold)
-                {digitalWrite(ledPin, HIGH);
+            if (filtered_value <= 30) 
+              {digitalWrite(ledPin, LOW);}
+            if (filtered_value >= 100) 
+              {digitalWrite(ledPin, HIGH);}
+            if (filtered_value <= 0) 
+              {filtered_value = 0;}
+            if (filtered_value >= 1023) 
+              {filtered_value = 1023;
+              digitalWrite(ledPin, HIGH);          
+              }
+              
+            //CocoMidi.send(MIDI_NOTEON,0, filtered_value);
+            CocoMidi.sendCCHires(filtered_value, (4*1)+1);
+            CocoMidi.sendCCHires(velocityValue[0]+500, (4*0)+1);
+            CocoMidi.sendCCHires(value[0], (4*2)+1);
+
+            if (filtered_value >= 10)
+                { 
                     if (note_off[0] == 1)
                         {
 #ifdef USE_MIDI
-                           // TeenyMidi.send(MIDI_NOTEON,0, filtered_value );
+                           // CocoMidi.send(MIDI_NOTEON,0, filtered_value );
 #endif
-
 #ifdef USE_KEYBOARD
                 keyCount++;
-                if (keyCount == keyTotal) 
-                  {keyCount = 0;}
-                CocoKeyboard.print(key[keyCount]);
+                if (keyCount == keyTotal){keyCount = 0;}
+                    digitalWrite(ledPin, HIGH);          
+
+                            CocoKeyboard.print(key[keyCount]);
 #endif
-                        note_off[0] = 0;
+                            note_off[0] = 0;
                         }
                 }
-            if (velocityValue[0]  <= 500 - velocityThreshold)
-              {digitalWrite(ledPin, HIGH);}
             else
-              {digitalWrite(ledPin, LOW);
-                 if (note_off[0] == 0) 
-                   {note_off[0] = 1;}
-               }
+                {    digitalWrite(ledPin, LOW);          
+
+                    if (note_off[0] == 0)
+                        {
+#ifdef USE_MIDI
+                            //CocoMidi.send(MIDI_NOTEOFF,0,127);
+#endif
+                            note_off[0] = 1;
+                        }
+                }
+
             previousMillis = millis();
         }
 
@@ -147,18 +160,19 @@ void loop()
     if (value[0] > 0) CocoTouchFilter_put(&CocoFilter[0], value[0]);
     
 #ifdef USE_MIDI
-    TeenyMidi.update();
+    CocoMidi.update();
 #endif
 
 #ifdef USE_MIDI
-    TeenyMidi.delay(1);
+    CocoMidi.delay(1);
 #endif
 
 #ifdef USE_KEYBOARD
+    //CocoKeyboard.update();
     CocoKeyboard.delay(1);
 #endif
 
-prevVelocity[0] = velocityValue[0];
-
+//velocityValue[0] = value[0]-prevValue[0];
+//prevValue[0] = value[0];
 }
 
